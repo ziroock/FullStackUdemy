39f303b46a6d887fb899f6c1d46ca78dd223fbdb
ziroock <zaprin.ignatiev@gmail.com>
2020-02-14 16:37:58 -0800

    modules & dev for vesrion control

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+//dev.js - DON'T COMMIT THIS!!!
+module.exports = {
+	// the clientID is a public token that can be shared with anyone
+	googleClientID: '1093768788787-qvlnr2am64mo1eo49msbuhkp5n7qv7me.apps.googleusercontent.com',
+	//clientSecret is something we don't want anyone to see
+	googleClientSecret: 'D8iaHsuHdV0MmL2VAPV0szcO',
+	mongoURI: 'mongodb+srv://bugzz:ypLociUExnRZmARJ@emaily-hhkhy.mongodb.net/test?retryWrites=true&w=majority',
+	cookieKey: 'vausdogvbjhfbvrwejhgtquyfqpqhfvmnbvcmnbvdcjahsdxfgjwefkw',
+	facebookClientID: '2568272993452373',
+	facebookClientSecret: '1c32006d558a6103fc5b4d9088f99e3d',
+};
