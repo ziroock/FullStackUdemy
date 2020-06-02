12d922086d1252c1bd83203fbbdeccc95bfa68b2
ziroock <zaprin.ignatiev@gmail.com>
2020-06-02 10:48:49 -0700

    Changes for herku deployment

--- index.js
+++ index.js
@@ -33,6 +33,21 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+// runs only in production
+if(process.env.NODE_ENV === 'production') {
+	// Express will serve up production assets
+	// like our main.js file, or main.css file!
+	app.use(express.static('client/build'));
+
+	// Express will serve up the index.html file
+	// if it doesn't recognize route
+	// a.k.a catch all request when the route or whatever is not found in client
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+
+}
 
 //environmental variable from Heroku or my own port
 const PORT = process.env.PORT || 5000;
