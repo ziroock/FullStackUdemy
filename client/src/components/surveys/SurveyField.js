6fa5e8b294ed062248982d1db2cb7cda73537bca
ziroock <zaprin.ignatiev@gmail.com>
2020-06-03 01:28:19 -0700

    Added input error checking, and displaying errors

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,12 +1,15 @@
 // SurveyField contains logic to render a single label and text input
 import React from 'react';
 
-export default ({ input, label }) => {
+export default ({ input, label, meta: {error, touched} }) => {
     return (
         //{...input} gets all the properties from input
         <div>
             <label>{label}</label>
-            <input {...input} />
+            <input {...input} style={{ marginBottom: '5px'}}/>
+            <div className="red-text" style={{ marginBottom: '15px', fontWeight: 'bold'}}>
+                {touched && error}
+            </div>
         </div>
     )
 }
--- client/src/components/surveys/SurveyFrom.js
+++ client/src/components/surveys/SurveyFrom.js
@@ -1,22 +1,29 @@
 // SurveyForm gets user input
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
-
 import { Link } from 'react-router-dom';
 import SurveyField from "./SurveyField";
+import validateEmails from "../../utills/validateEmails";
 
 const FIELDS = [
-    {label: 'Survey Title', name: 'title'},
-    {label: 'Subject Line', name: 'subject'},
-    {label: 'Email Body', name: 'body'},
-    {label: 'Recipient List', name: 'emails'}
+    {label: 'Survey Title', name: 'title', errorMsg: 'Please provide a Survey Title!'},
+    {label: 'Subject Line', name: 'subject', errorMsg: 'Please provide a Subject!'},
+    {label: 'Email Body', name: 'body', errorMsg: 'Please provide a Body!'},
+    {label: 'Recipient List', name: 'emails', errorMsg: 'Please provide Recipient Emails!'}
 ];
 
 class SurveyForm extends Component {
     renderFields() {
         // ({label, name}) same as (field) and then using field.label and field.name
         return FIELDS.map(({label, name}) => {
-           return( <Field key={name} component={SurveyField} type="text" label={label} name={name}/> );
+           return(
+               <Field
+                   key={name}
+                   component={SurveyField}
+                   type="text"
+                   label={label} name={name}
+               />
+           );
         });
     }
 
@@ -39,6 +46,22 @@ class SurveyForm extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    errors.emails = validateEmails(values.emails || '');
+
+    FIELDS.forEach( ({ name, errorMsg }) => {
+        if(!values[name]) {
+            errors[name] = errorMsg;
+        }
+    });
+
+    return errors;
+}
+
+//validate is the same as validate: validate
 export default reduxForm({
+    validate,
     form: 'surveyForm'
 })(SurveyForm);
