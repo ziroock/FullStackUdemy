2a6a5c5137ee173eb8cd3595c498dbb1ed650d8b
ziroock <zaprin.ignatiev@gmail.com>
2020-06-04 12:00:49 -0700

    Wired React to Redux!

--- client/src/reducers/surveysReducer.js
+++ client/src/reducers/surveysReducer.js
@@ -1,4 +1,4 @@
-import {FETCH_SURVEYS, FETCH_USER} from '../actions/types';
+import { FETCH_SURVEYS } from '../actions/types';
 
 export default function(state = [], action) {
     switch (action.type) {
