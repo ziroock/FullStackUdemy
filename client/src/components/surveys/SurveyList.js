38364252cc4547d023155ff877737dcfb90ceb63
ziroock <zaprin.ignatiev@gmail.com>
2020-06-04 12:11:45 -0700

    Rendering the surveysList and added materialize card component.

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,10 +7,30 @@ class SurveyList extends Component {
         this.props.fetchSurveys();
     }
 
+    renderSurveys() {
+        return this.props.surveys.map(survey => {
+           return (
+               <div className="card blue-grey" key={survey._id}>
+                   <div className="card-content white-text">
+                       <span className="card-title">{survey.title}</span>
+                       <p>{survey.body}</p>
+                       <p className="right">
+                           Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+                       </p>
+                   </div>
+                   <div className="card-action">
+                       <a>Yes: {survey.yes}</a>
+                       <a>NO: {survey.no}</a>
+                   </div>
+               </div>
+           );
+        });
+    }
+
     render() {
         return (
           <div>
-              SurveyList
+              {this.renderSurveys()}
           </div>
         );
     }
