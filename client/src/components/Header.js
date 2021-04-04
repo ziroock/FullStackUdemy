aab057fa828f834a18414c894595c14e296390f1
ziroock <zaprin.ignatiev@gmail.com>
2021-04-03 23:54:35 -0700

    added directions

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -24,6 +24,7 @@ class Header extends Component {
     render() {
         //console.log(this.props);
         return (
+            <React.Fragment>
             <nav>
                 <div className="nav-wrapper" style={{margin: '0 15px'}}>
                     <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">
@@ -34,6 +35,23 @@ class Header extends Component {
                     </ul>
                 </div>
             </nav>
+                <div className={'directions'}
+                style={{
+                    position: 'absolute',
+                    top: '300px',
+                    right: '50px',
+                    width: '200px',
+                    background: '#8097A2',
+                    borderRadius: '5px',
+                    padding: '10px',
+                    color: 'white',
+                    zIndex: '5'}}>
+                    <div style={{textAlign: 'center'}}>NOTE</div>
+                    To add credits use the
+                    following card number 4242 4242 4242 4242, any 3 digit CVC and an expiration
+                    date that is in the future from the day you are using the application.
+                </div>
+            </React.Fragment>
         );
     }
 }
